c36078d018373786f124d67a3c484f200e9dfc31
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-03-11 16:27:00 +0100

    task-1-i

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+intervalId = null;
+
+startButton.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.diisabled = false;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+
+  startButton.disabled = false;
+  stopButton.diisabled = true;
+});
